6f19996e3c28cbef4b69350e6426cde1404079a3
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-23 01:16:37 +0530

    add sessions resourse

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { SessionsModule } from './sessions/sessions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule, UsersModule, SessionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
