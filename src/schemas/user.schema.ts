349149752b4db78191084f7a26b8fb4798fea209
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-25 01:31:10 +0530

    add user schema and jwt service

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,22 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true, trim: true })
+  name: string;
+
+  @Prop({ required: true, unique: true, lowercase: true, trim: true })
+  email: string;
+
+  @Prop({ required: true })
+  password: string;
+
+  @Prop({ required: true, default: false })
+  isPasswordVerified: boolean;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
+
+export type UserDocument = HydratedDocument<User>;
+export const USER_MODEL = User.name;
