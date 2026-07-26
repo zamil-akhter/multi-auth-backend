8a5dec430e3d81bb88451379228fd43812f1b9f1
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-27 00:45:27 +0530

    created a sample api

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { USER_MODEL, UserSchema } from 'src/schemas/user.schema';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: USER_MODEL, schema: UserSchema }])],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,18 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/user.dto';
+import { USER_MODEL, UserDocument } from 'src/schemas/user.schema';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(@InjectModel(USER_MODEL) private readonly userModel: Model<UserDocument>) {}
+
+  async create(createUserDto: CreateUserDto) {
+    return this.userModel.create(createUserDto);
   }
 
-  findAll() {
-    return `This action returns all users`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findAll() {
+    return this.userModel.find().exec();
   }
 }
