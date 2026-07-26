0c2e62b1b0f3b78cbc83b82386eba8d6960ce849
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-27 01:01:28 +0530

    added bcrypt

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,13 +3,23 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/user.dto';
 import { USER_MODEL, UserDocument } from 'src/schemas/user.schema';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(USER_MODEL) private readonly userModel: Model<UserDocument>) {}
 
   async create(createUserDto: CreateUserDto) {
-    return this.userModel.create(createUserDto);
+    const { name, email, password } = createUserDto;
+
+    const isUserExists = await this.userModel.findOne({ email });
+    if (isUserExists) {
+      throw new Error('User with this email already exists.');
+    }
+    
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return this.userModel.create({ name, email, password: hashedPassword });
   }
 
   async findAll() {
