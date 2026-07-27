05d68bface8440df009c0eec1b3f7fc42cc42938
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-27 23:19:45 +0530

    remove authentication api from users module

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,14 +5,4 @@ import { CreateUserDto } from './dto/user.dto';
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
-
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,21 +8,4 @@ import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(USER_MODEL) private readonly userModel: Model<UserDocument>) {}
-
-  async create(createUserDto: CreateUserDto) {
-    const { name, email, password } = createUserDto;
-
-    const isUserExists = await this.userModel.findOne({ email });
-    if (isUserExists) {
-      throw new Error('User with this email already exists.');
-    }
-    
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    return this.userModel.create({ name, email, password: hashedPassword });
-  }
-
-  async findAll() {
-    return this.userModel.find().exec();
-  }
 }
