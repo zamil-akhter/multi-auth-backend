51fe43f4b6e9670579f04378d1ec9783f86313f5
Zamil Akhter <zamilakhter.rk@gmail.com>
2026-07-27 23:39:55 +0530

    fix 500 error and optimise the api

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,25 +4,22 @@ import { USER_MODEL, UserDocument } from 'src/schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import * as bcrypt from 'bcrypt';
+import { messages } from 'src/common/helpers/message';
 
 @Injectable()
 export class AuthService {
   constructor(@InjectModel(USER_MODEL) private readonly userModel: Model<UserDocument>) {}
-  async signUp(dto: SignUpDto): Promise<{ success: boolean; message: string; data?: any }> {
+  async signUp(dto: SignUpDto): Promise<{ success: boolean; message: string; data?: SignUpDto }> {
     const { name, email, password } = dto;
 
     const isUserExists = await this.userModel.findOne({ email });
     if (isUserExists) {
-      throw new Error('User with this email already exists.');
+      return { success: false, message: messages.USER_ALREADY_EXISTS };
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    this.userModel.create({ name, email, password: hashedPassword });
+    await this.userModel.create({ name, email, password: hashedPassword });
 
-    return {
-      success: true,
-      message: 'User signed up successfully',
-      data: {},
-    };
+    return { success: true, message: messages.USER_SIGNUP_SUCCESS };
   }
 }
